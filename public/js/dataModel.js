abc399fb01ea858bc6255dd2fb9961db327dc655
Michael Thompson <mroythompson@gmail.com>
2014-10-05 21:51:15 -0700

    Adding dataModel module for tracking sudoku board data, using some underscore.js helpers

---
+++ public/js/dataModel.js
@@ -0,0 +1,70 @@
+DataModel = (function() {
+  function DataModel(data) {
+    this.dataMatrix = data;
+  }
+
+  DataModel.prototype.isBoardCorrect = function() {
+    var rowsCorrect = evalArrayOfSets(this.dataMatrix);
+    var columnsCorrect = evalArrayOfSets(getColumns(this.dataMatrix));
+    var squaresCorrect = evalArrayOfSets(getSquares(this.dataMatrix));
+    return rowsCorrect && columnsCorrect && squaresCorrect;
+  }
+
+  function getColumns(dataMatrix) {
+    var columns = [[],[],[],[],[],[],[],[],[]];
+    _.each(dataMatrix, function(row) {
+      _(9).times(function(i) {
+        columns[i].push(row[i]);
+      });
+    });
+
+    return columns;
+  }
+
+  function getSquares(dataMatrix) {
+    var squareSets = [[],[],[],[],[],[],[],[],[]];
+    
+    _.each(dataMatrix, function(row, i) {
+      var start = 0;
+      var stop = 3;
+      var addee = 0;
+      // TODO: Clean & simplify 'addee'
+      if (i >= 6) {
+        addee = 6;
+      } else if (i >= 3) {
+        addee = 3;
+      }
+
+      _(3).times(function(n) {
+        squareSets[n + addee] = squareSets[n + addee].concat(row.slice(start, stop));
+        start = start + 3;
+        stop = stop + 3;
+      });
+    });
+
+    return squareSets;
+  }
+
+  function evalArrayOfSets(setArray) {
+    return _.reduce(setArray, function(memo, set) { return memo && evalSet(set); }, true);
+  }
+
+  // Returns true if the given array is a set of the numbers 1-9, occurring once each
+  function evalSet(intArray) {
+    if (intArray.length !== 9) {
+      return false;
+    }
+    var numberSet = [];
+    result = _.every(intArray, function(n) {
+      var isValidInput = (n > 0 && n < 10);
+      var isUnique = !(n in numberSet);
+      numberSet[n] = true;
+
+      return isValidInput && isUnique;
+    });
+
+    return result;
+  }
+
+  return DataModel;
+}());
--- public/js/sudoku.js
+++ public/js/sudoku.js
@@ -1,12 +1,28 @@
-var SudokuGame = (function() {
+SudokuGame = (function() {
   var game = {};
 
   game.start = function() {
-    this.setupTemplates()
-    this.setupBoard()
+    setupDataModel();
+    setupBoardView();
   }
 
-  game.setupTemplates = function() {
+  function setupDataModel() {
+    game.dataModel = new DataModel(getInitialBoardData());
+  }
+
+  function setupBoardView() {
+    setupTemplates();
+    var template = Handlebars.compile(Templates.sudokuBoard);
+    var result = template(getInitialBoardData());
+    $('#sudoku-board').append(result);
+
+    $('#submit-button').click( function(ev) {
+      ev.preventDefault()
+      submitBoard();
+    })
+  }
+
+  function setupTemplates() {
     Templates = {}
     Templates.sudokuBoard = $('#board-template').html()
     Templates._row = $('#row-template').html()
@@ -16,23 +32,33 @@ var SudokuGame = (function() {
     Handlebars.registerPartial('_box', Templates._box)
   }
 
-  game.setupBoard = function() {
-    var template = Handlebars.compile(Templates.sudokuBoard);
-    var result = template(this._getBoardData());
-    $('#sudoku-board').append(result);
+  function submitBoard() {
+    if (game.dataModel.isBoardCorrect()) {
+      submitSuccess();
+    } else {
+      submitFail();
+    }
   }
 
-  game._getBoardData = function() {
+  function submitSuccess() {
+    console.log('SUCCESS');
+  }
+
+  function submitFail() {
+    console.log('FAIL');
+  }
+
+  function getInitialBoardData() {
     return [
-      [5,3,null,null,7,null,null,null, null],
-      [6, null, null, 1, 9, 5, null, null, null],
-      [null, 9, 8, null, null, null, null, 6, null],
-      [8,null, null, null, 6, null, null, null, 3],
-      [4, null, null, 8, null, 3, null, null, 1],
-      [7, null, null, null, 2, null, null, null, 6],
-      [null, 6, null, null, null, null, 2, 8, null],
-      [null, null, null, 4,1,9, null,null,5],
-      [null,null,null, null, 8, null, null, 7,9]
+      [5,3,4,6,7,8,9,1,2],
+      [6,7,2,1,9,5,3,4,8],
+      [1,9,8,3,4,2,5,6,7],
+      [8,5,9,7,6,1,4,2, null],
+      [4,2,6,8,5,3,7,9,1],
+      [7,1,3,9,2,4,8,5,6],
+      [9,6,1,5,3,7,2,8,4],
+      [2,8,7,4,1,9,6,3,5],
+      [3,4,5,2,8,6,1,7,9]
     ];
   }
 
